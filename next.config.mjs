5f221813abc4204c2d117cf9b1eff51c36cb7e6e
Kipyegon Lagat <kipyegongithub@gmail.com>
2025-08-14 23:23:21 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
-    ignoreDuringBuilds: tr,
+    ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: true,
